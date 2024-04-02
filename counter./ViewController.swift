4b5ec00672c658f55c595c7df76dec76e494f4e9
serg <gerasichev.serg@gmail.com>
2024-04-02 21:23:41 +0300

    first commit

---
+++ counter./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counter.
+//
+//  Created by Герасичев Сергей on 02.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
